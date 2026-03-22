3ec75a6ead01f4a2cc3d836163d398b2d86539ea
aryanmethodd-creator <aryanmethodd@gmail.com>
2026-03-22 14:00:16 +0530

    script.js

--- script.js
+++ script.js
@@ -3,38 +3,43 @@ const name = params.get("name") || "";
 
 function start() {
 
-    // MUSIC
-    document.getElementById("bgMusic").play();
+    try {
+        // MUSIC
+        document.getElementById("bgMusic").play();
 
-    // PHOTO SHOW
-    setTimeout(() => {
-        document.querySelector(".photo").style.opacity = "1";
-    }, 500);
+        // PHOTO
+        setTimeout(() => {
+            document.querySelector(".photo").style.opacity = "1";
+        }, 500);
 
-    // CANDLE GLOW
-    setTimeout(() => {
-        document.querySelector(".candle").innerText = "🕯️✨";
-    }, 1500);
+        // CANDLE
+        setTimeout(() => {
+            document.querySelector(".candle").innerText = "🕯️✨";
+        }, 1500);
 
-    // CAKE SHOW
-    setTimeout(() => {
-        document.querySelector(".cake").style.display = "block";
-    }, 2500);
+        // CAKE SHOW
+        setTimeout(() => {
+            document.querySelector(".cake").style.display = "block";
+        }, 2500);
 
-    // CAKE CUT
-    setTimeout(() => {
-        document.querySelector(".cake").innerText = "🍰";
-    }, 4000);
+        // CAKE CUT
+        setTimeout(() => {
+            document.querySelector(".cake").innerText = "🍰";
+        }, 4000);
 
-    // LETTER SHOW
-    setTimeout(() => {
-        let msg = "🎂 Happy Birthday " + name + " 💖\n\n"
-        + "You are very special to me 😊\n"
-        + "Stay happy always 💕\n"
-        + "Keep smiling ❤️";
+        // LETTER
+        setTimeout(() => {
+            let msg = "🎂 Happy Birthday " + name + " 💖\n\n"
+            + "You are very special to me 😊\n"
+            + "Stay happy always 💕\n"
+            + "Keep smiling ❤️";
 
-        let letter = document.getElementById("letter");
-        letter.innerText = msg;
-        letter.style.opacity = "1";
-    }, 5500);
+            let letter = document.getElementById("letter");
+            letter.innerText = msg;
+            letter.style.opacity = "1";
+        }, 5500);
+
+    } catch (e) {
+        alert("Error: " + e);
+    }
 }
