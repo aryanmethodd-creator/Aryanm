4b4ac966f1b91dbd4a882fe2715a58360c95b457
aryanmethodd-creator <aryanmethodd@gmail.com>
2026-03-22 14:09:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-const params = new URLSearchParams(window.location.search);
-const name = params.get("name") || "";
+  function start() {
 
-function start() {
+    // MUSIC
+    document.getElementById("bgMusic").play();
 
-    try {
-        // MUSIC
-        document.getElementById("bgMusic").play();
+    // SHOW SCENE
+    document.getElementById("scene").style.display = "block";
+    document.getElementById("startText").style.display = "none";
 
-        // PHOTO
-        setTimeout(() => {
-            document.querySelector(".photo").style.opacity = "1";
-        }, 500);
+    // PHOTO
+    setTimeout(() => {
+        document.querySelector(".photo").style.opacity = "1";
+    }, 500);
 
-        // CANDLE
-        setTimeout(() => {
-            document.querySelector(".candle").innerText = "🕯️✨";
-        }, 1500);
+    // CANDLE
+    setTimeout(() => {
+        document.querySelector(".candle").innerText = "🕯️✨";
+    }, 1500);
 
-        // CAKE SHOW
-        setTimeout(() => {
-            document.querySelector(".cake").style.display = "block";
-        }, 2500);
+    // CAKE
+    setTimeout(() => {
+        document.querySelector(".cake").style.display = "block";
+    }, 2500);
 
-        // CAKE CUT
-        setTimeout(() => {
-            document.querySelector(".cake").innerText = "🍰";
-        }, 4000);
+    // CAKE CUT
+    setTimeout(() => {
+        document.querySelector(".cake").innerText = "🍰";
+    }, 4000);
 
-        // LETTER
-        setTimeout(() => {
-            let msg = "🎂 Happy Birthday " + name + " 💖\n\n"
-            + "You are very special to me 😊\n"
-            + "Stay happy always 💕\n"
-            + "Keep smiling ❤️";
-
-            let letter = document.getElementById("letter");
-            letter.innerText = msg;
-            letter.style.opacity = "1";
-        }, 5500);
-
-    } catch (e) {
-        alert("Error: " + e);
-    }
+    // LETTER
+    setTimeout(() => {
+        document.getElementById("letter").innerText =
+        "🎂 Happy Birthday 💖\n\nYou are very special 😊\nStay happy always 💕";
+        document.getElementById("letter").style.opacity = "1";
+    }, 5500);
 }
