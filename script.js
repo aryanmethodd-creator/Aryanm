e78d6df484f0d6b97550bad2733d7ceff4d1eb3f
aryanmethodd-creator <aryanmethodd@gmail.com>
2026-03-22 13:48:47 +0530

    script.js

--- script.js
+++ script.js
@@ -2,53 +2,39 @@ const params = new URLSearchParams(window.location.search);
 const name = params.get("name") || "";
 
 function start() {
-    // Music play
-    const music = document.getElementById("bgMusic");
-    if (music) music.play();
 
-    // Hide start text
-    const startText = document.getElementById("startText");
-    if (startText) startText.style.display = "none";
-
-    // Show main scene
-    const scene = document.getElementById("scene");
-    if (scene) scene.style.display = "block";
+    // MUSIC
+    document.getElementById("bgMusic").play();
 
     // PHOTO SHOW
     setTimeout(() => {
-        const photo = document.querySelector(".photo");
-        if (photo) photo.style.opacity = "1";
+        document.querySelector(".photo").style.opacity = "1";
     }, 500);
 
     // CANDLE GLOW
     setTimeout(() => {
-        const candle = document.querySelector(".candle");
-        if (candle) candle.innerText = "🕯️✨";
+        document.querySelector(".candle").innerText = "🕯️✨";
     }, 1500);
 
     // CAKE SHOW
     setTimeout(() => {
-        const cake = document.querySelector(".cake");
-        if (cake) cake.style.display = "block";
+        document.querySelector(".cake").style.display = "block";
     }, 2500);
 
     // CAKE CUT
     setTimeout(() => {
-        const cake = document.querySelector(".cake");
-        if (cake) cake.innerText = "🍰";
+        document.querySelector(".cake").innerText = "🍰";
     }, 4000);
 
     // LETTER SHOW
     setTimeout(() => {
-        const letter = document.getElementById("letter");
-        if (letter) {
-            let message = "🎂 Happy Birthday " + Diya + " 💖\n\n"
-                + "You are very special to me 😊\n"
-                + "May your day be full of happiness 💕\n"
-                + "Keep smiling always ❤️";
+        let msg = "🎂 Happy Birthday " + Diya + " 💖\n\n"
+        + "You are very special to me 😊\n"
+        + "Stay happy always 💕\n"
+        + "Keep smiling ❤️";
 
-            letter.innerText = message;
-            letter.style.opacity = "1";
-        }
+        let letter = document.getElementById("letter");
+        letter.innerText = msg;
+        letter.style.opacity = "1";
     }, 5500);
 }
