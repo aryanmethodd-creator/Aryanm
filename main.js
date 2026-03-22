c40405fd97d84aa655340f38719c44ed0b80bb3c
aryanmethodd-creator <aryanmethodd@gmail.com>
2026-03-22 14:25:21 +0530

    main.js

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-  function start() {
+function start() {
 
-    // MUSIC
-    document.getElementById("bgMusic").play();
+  // MUSIC
+  const music = document.getElementById("bgMusic");
+  if (music) music.play();
 
-    // SHOW SCENE
-    document.getElementById("scene").style.display = "block";
-    document.getElementById("startText").style.display = "none";
+  // SHOW SCENE
+  document.getElementById("scene").style.display = "block";
+  document.getElementById("startText").style.display = "none";
 
-    // PHOTO
-    setTimeout(() => {
-        document.querySelector(".photo").style.opacity = "1";
-    }, 500);
+  // PHOTO
+  setTimeout(() => {
+    document.querySelector(".photo").style.opacity = "1";
+  }, 500);
 
-    // CANDLE
-    setTimeout(() => {
-        document.querySelector(".candle").innerText = "🕯️✨";
-    }, 1500);
+  // CANDLE
+  setTimeout(() => {
+    document.querySelector(".candle").innerText = "🕯️✨";
+  }, 1500);
 
-    // CAKE
-    setTimeout(() => {
-        document.querySelector(".cake").style.display = "block";
-    }, 2500);
+  // CAKE
+  setTimeout(() => {
+    document.querySelector(".cake").style.display = "block";
+  }, 2500);
 
-    // CAKE CUT
-    setTimeout(() => {
-        document.querySelector(".cake").innerText = "🍰";
-    }, 4000);
+  // CAKE CUT
+  setTimeout(() => {
+    document.querySelector(".cake").innerText = "🍰";
+  }, 4000);
 
-    // LETTER
-    setTimeout(() => {
-        document.getElementById("letter").innerText =
-        "🎂 Happy Birthday 💖\n\nYou are very special 😊\nStay happy always 💕";
-        document.getElementById("letter").style.opacity = "1";
-    }, 5500);
+  // LETTER
+  setTimeout(() => {
+    document.getElementById("letter").innerText =
+      "🎂 Happy Birthday 💖\n\nYou are very special 😊\nStay happy always 💕";
+    document.getElementById("letter").style.opacity = "1";
+  }, 5500);
 }
